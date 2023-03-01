83d3a6c14b18dd1052a741d536657f83cfa0c852
Paula de Freitas Camargos <paulacamargos26@gmail.com>
2023-03-01 15:33:21 -0300

    -m "new commits"

--- q05/algBool.java
+++ q05/algBool.java
@@ -7,6 +7,9 @@ class algBool {
     estudo do pub.in e pub.out: saída precisa retornar 1 ou 0.
     cada entrada recebe um inteiro a indicando o número de entradas da expressão:
     ex 1. 
-    /* 2 (num entrada) - 0 0 (expressão) - (not(A), not(B))
-    o inverso de 0, que é false, é 1 = true. resultado = 1;
+    2 (num entrada) - 0 0 (expressão) - (not(A), not(B))
+    o inverso de 0, que é false, é 1 = true. resultado = 1
+    */
+
+    
 }
