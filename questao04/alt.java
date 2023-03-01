c7d3b305fc0742b5683c534b2751a3c886807c25
Paula de Freitas Camargos <paulacamargos26@gmail.com>
2023-03-01 00:26:40 -0300

    commit

---
+++ questao04/alt.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class altAleatoria {
+    public static void main(String[] args) {
+        String palavra = "", palavraAlt; 
+        Random gerador = new Random( );
+        Boolean fim = false; 
+        char a, b, sorteado;
+
+        gerador.setSeed (4);
+
+        while (!fim) {
+            palavra = MyIO.readLine();
+            palavraAlt = "";
+
+            if (palavra.charAt(0) == 'F' && palavra.charAt(1) == 'I' && palavra.charAt(2) == 'M') {
+                fim = true;
+            }
+
+            else {
+                a = (char) ('a' + Math.abs(gerador.nextInt()) % 26); //sorteio das duas letras minúsculas
+                b = (char) ('a' + Math.abs(gerador.nextInt()) % 26);
+
+                for (int i=0; i<palavra.length(); i++) {
+                    sorteado = palavra.charAt(i); 
+
+                    if (a == sorteado) {
+                        palavraAlt+=b; 
+                    }
+                    else if (b == sorteado) {
+                        palavraAlt+=a; 
+                    }
+
+                    else {
+                        palavraAlt+=palavra.charAt(i); 
+                    }
+                }
+
+                MyIO.println(palavraAlt);
+            }
+        }
+    }
+}
