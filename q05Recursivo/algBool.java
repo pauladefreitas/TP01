05800bf28cd34e33181c668e372164014061fb2a
Paula de Freitas Camargos <paulacamargos26@gmail.com>
2023-03-02 09:03:23 -0300

    add recursividade

---
+++ q05Recursivo/algBool.java
@@ -0,0 +1,15 @@
+class algBool {
+    /*
+    objetivo: criar método iterativo que recebe uma string contendo uma e. booleana 
+    e o valor de suas entradas e retorna um bool indicando se a expressão é verdadeira
+    ou falsa. 
+
+    estudo do pub.in e pub.out: saída precisa retornar 1 ou 0.
+    cada entrada recebe um inteiro a indicando o número de entradas da expressão:
+    ex 1. 
+    2 (num entrada) - 0 0 (expressão) - (not(A), not(B))
+    o inverso de 0, que é false, é 1 = true. resultado = 1
+    */
+
+    
+}
